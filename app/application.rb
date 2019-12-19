a1b752d0a90fc728c52141554bf0e9865353f430
ncaudill27 <nelson.a.caudill@gmail.com>
2019-12-19 08:50:16 -0500

    Done.

---
+++ app/application.rb
@@ -0,0 +1,26 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+
+      item = req.path.split('/items/').last
+      item = Item.find_item_by_name(item)
+
+      if item
+        
+        resp.write item.price
+      else
+        resp.write 'Item not found'
+        resp.status = 400
+      end
+    else
+      resp.write 'Route not found'
+      resp.status = 404
+    end
+
+    resp.finish
+  end
+end
--- app/item.rb
+++ app/item.rb
@@ -1,8 +1,19 @@
 class Item
   attr_accessor :name, :price
 
+  @@all = []
+  
   def initialize(name,price)
     @name = name
     @price = price
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_item_by_name(name)
+    @@all.find{|item| item.name == name }
   end
 end
